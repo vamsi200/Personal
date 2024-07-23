d07db272a3175655e55f35cc60365ecca18dfbf8
vamsi200 <vamsivanama891@gmail.com>
2024-07-23 23:04:40 +0530

    Update install_tools script

--- tools/install_tools/target/debug/deps/install_tools-6739bdefdc218c5c.d
+++
@@ -1,5 +0,0 @@
-/home/vamsi/scripts/tools/install_tools/target/debug/deps/install_tools-6739bdefdc218c5c: src/main.rs
-
-/home/vamsi/scripts/tools/install_tools/target/debug/deps/install_tools-6739bdefdc218c5c.d: src/main.rs
-
-src/main.rs:
--- tools/install_tools/target/debug/deps/number_prefix-8ca6c91fd8c559d0.d
+++
@@ -1,8 +0,0 @@
-/home/vamsi/scripts/tools/install_tools/target/debug/deps/libnumber_prefix-8ca6c91fd8c559d0.rmeta: /home/vamsi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/number_prefix-0.4.0/src/lib.rs /home/vamsi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/number_prefix-0.4.0/src/parse.rs
-
-/home/vamsi/scripts/tools/install_tools/target/debug/deps/libnumber_prefix-8ca6c91fd8c559d0.rlib: /home/vamsi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/number_prefix-0.4.0/src/lib.rs /home/vamsi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/number_prefix-0.4.0/src/parse.rs
-
-/home/vamsi/scripts/tools/install_tools/target/debug/deps/number_prefix-8ca6c91fd8c559d0.d: /home/vamsi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/number_prefix-0.4.0/src/lib.rs /home/vamsi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/number_prefix-0.4.0/src/parse.rs
-
-/home/vamsi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/number_prefix-0.4.0/src/lib.rs:
-/home/vamsi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/number_prefix-0.4.0/src/parse.rs:
