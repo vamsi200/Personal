6eb8b6db87126edcdc12a2c11126d76dd13b2c0a
vamsi200 <vamsivanama891@gmail.com>
2024-07-23 23:04:40 +0530

    Update install_tools script

---
+++ install_tools/target/debug/deps/install_tools-4a9ede3a0aace405.d
@@ -0,0 +1,5 @@
+/home/vamsi/scripts/tools/install_tools/target/debug/deps/install_tools-4a9ede3a0aace405: src/main.rs
+
+/home/vamsi/scripts/tools/install_tools/target/debug/deps/install_tools-4a9ede3a0aace405.d: src/main.rs
+
+src/main.rs:
---
+++ install_tools/target/debug/deps/install_tools-6739bdefdc218c5c.d
@@ -0,0 +1,5 @@
+/home/vamsi/scripts/tools/install_tools/target/debug/deps/install_tools-6739bdefdc218c5c: src/main.rs
+
+/home/vamsi/scripts/tools/install_tools/target/debug/deps/install_tools-6739bdefdc218c5c.d: src/main.rs
+
+src/main.rs:
