fb2d41fe81d252506a94a011d6ce129a956d507e
Vamshi V <66506614+vamsi200@users.noreply.github.com>
2024-07-23 23:09:23 +0530

    Delete install_tools directory

--- install_tools/target/debug/deps/install_tools-4a9ede3a0aace405.d
+++
@@ -1,5 +0,0 @@
-/home/vamsi/scripts/tools/install_tools/target/debug/deps/install_tools-4a9ede3a0aace405: src/main.rs
-
-/home/vamsi/scripts/tools/install_tools/target/debug/deps/install_tools-4a9ede3a0aace405.d: src/main.rs
-
-src/main.rs:
--- install_tools/target/debug/deps/install_tools-a3ea27dbcd51f885.d
+++
@@ -1,5 +0,0 @@
-/home/vamsi/scripts/tools/install_tools/target/debug/deps/libinstall_tools-a3ea27dbcd51f885.rmeta: src/main.rs
-
-/home/vamsi/scripts/tools/install_tools/target/debug/deps/install_tools-a3ea27dbcd51f885.d: src/main.rs
-
-src/main.rs:
