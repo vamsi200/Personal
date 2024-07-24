d0ae4de2972305ace34aaa6b9716980d0419e0bf
vamsi200 <vamsivanama891@gmail.com>
2024-07-24 15:47:07 +0530

    Added tmux configs

--- install_tools/target/debug/deps/install_tools-b5571c0791a428c8.d
+++
@@ -1,5 +0,0 @@
-/home/vamsi/scripts/tools/install_tools/target/debug/deps/libinstall_tools-b5571c0791a428c8.rmeta: src/main.rs
-
-/home/vamsi/scripts/tools/install_tools/target/debug/deps/install_tools-b5571c0791a428c8.d: src/main.rs
-
-src/main.rs:
--- install_tools/target/debug/install_tools.d
+++
@@ -1,1 +0,0 @@
-/home/vamsi/scripts/tools/install_tools/target/debug/install_tools: /home/vamsi/scripts/tools/install_tools/src/main.rs
