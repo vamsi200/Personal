2683e15b22e535311525a52c545fd09704c17733
vamsi200 <vamsivanama891@gmail.com>
2024-07-27 22:25:13 +0530

    Added functionality to open files, taken as an argument

---
+++ tools/fzf/target/debug/deps/fzf-6d046d59e3cd5f48.d
@@ -0,0 +1,5 @@
+/home/vamsi/scripts/Personal/tools/fzf/target/debug/deps/fzf-6d046d59e3cd5f48: src/main.rs
+
+/home/vamsi/scripts/Personal/tools/fzf/target/debug/deps/fzf-6d046d59e3cd5f48.d: src/main.rs
+
+src/main.rs:
