0e2b15584b663f84a6f2b2122bd002dc83f4f691
vamsi200 <vamsivanama891@gmail.com>
2024-08-05 16:24:36 +0530

    Modified install script

--- tools/install_tools/src/main.rs
+++ tools/install_tools/src/main.rs
@@ -45,15 +45,15 @@ fn main() {
             eprintln!("[*] Error occurred while installing tools: {}", e);
             process::exit(1);
         } else {
-            println!("[*] All tools installed successfully! ")
+            println!("[*] All tools installed successfully!");
         }
 
         if let Err(e) = install_nvchad() {
             eprintln!("[*] Error while installing Nvchad: {}", e);
             process::exit(1);
+        } else {
+            println!("[*] Nvchad installed successfully.\n");
         }
-
-        println!("[*] Nvchad installed successfully.\n");
     } else {
         println!("[*] Installation cancelled.");
         process::exit(0);
@@ -70,7 +70,7 @@ fn update_db() -> io::Result<ExitStatus> {
 }
 
 fn install_nvchad() -> io::Result<ExitStatus> {
-    println!("[*] Do you want to install NvChad? (y,n):");
+    print!("> Do you want to install NvChad? (y,n): ");
     io::stdout().flush().expect("[*] Failed to flush stdout");
 
     let mut input = String::new();
@@ -80,6 +80,7 @@ fn install_nvchad() -> io::Result<ExitStatus> {
     let input = input.trim().to_lowercase();
 
     if input == "y" {
+        println!("[*] Cloning NvChad Repo..");
         let nvchad_status = Command::new("git")
             .arg("clone")
             .arg("https://github.com/NvChad/starter")
@@ -120,19 +121,22 @@ fn install_tools(tools: &[&str]) -> io::Result<ExitStatus> {
                     pb.inc(1);
                 } else {
                     pb.println(format!("[*] Failed to install {}", tool));
-                    pb.finish_and_clear();
+                    pb.finish(); // Ends the progress bar
+                    println!("[*] Installation failed. See the logs for more details.");
                     return Err(io::Error::new(io::ErrorKind::Other, "Failed to install"));
                 }
             }
             Err(e) => {
                 pb.println(format!("[*] Error: {}", e));
-                pb.finish_and_clear();
+                pb.finish(); // Ends the progress bar
+                println!("[*] An error occurred. See the logs for more details.");
                 return Err(e);
             }
         }
     }
 
-    //pb.finish_with_message("\n[*] All tools installed successfully.");
+    pb.finish(); // End the progress bar
+    println!("\n[*] All tools installed successfully!");
 
     Ok(ExitStatus::from_raw(0))
 }
