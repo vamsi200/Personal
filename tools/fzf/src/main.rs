69d37f413102dec20b2d44643cf739e565bf314f
vamsi200 <vamsivanama891@gmail.com>
2024-10-03 20:21:08 +0530

    Added Xclip & improved the code

--- tools/fzf/src/main.rs
+++ tools/fzf/src/main.rs
@@ -1,8 +1,7 @@
-use std::collections::HashMap;
 use std::io::{self, Write};
 use std::path::Path;
 use std::process::Command;
-use std::{env, fs};
+use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -27,22 +26,31 @@ fn main() {
 
         if let Some(parent) = Path::new(&selected_file).parent() {
             let parent_path = format!("\"{}\"", parent.display().to_string());
+            if let Err(e) = copy_path(&parent_path) {
+                eprintln!("ERROR: {}", e);
+            }
             println!("[*] File DIR - {}", parent_path);
-            let _ = save_file_path(&parent_path);
         }
+
         if let Err(e) = open_or_prompt(&selected_file) {
             eprintln!("ERROR: {}", e);
         }
     }
 }
 
-fn save_file_path(file: &str) -> io::Result<()> {
-    fs::write("/tmp/file_path.txt", file)?;
-    Ok(())
-}
-
 fn open_file(file: &str) -> io::Result<()> {
     open_or_prompt(file)
+}
+
+fn copy_path(path: &str) -> io::Result<()> {
+    Command::new("sh")
+        .arg("-c")
+        .arg(format!("echo {} | xclip -selection clipboard", path))
+        .status()
+        .map_err(|e| {
+            io::Error::new(io::ErrorKind::Other, format!("Couldn't send the path to xclip: {}", e))
+        })?;
+    Ok(())
 }
 
 fn open_or_prompt(file: &str) -> io::Result<()> {
@@ -62,36 +70,19 @@ fn open_or_prompt(file: &str) -> io::Result<()> {
     Ok(())
 }
 
+
 fn determine_application(extension: &str) -> Option<&'static str> {
-    let mut apps = HashMap::new();
-    apps.insert("pdf", "zathura");
-    apps.insert("jpg", "eog");
-    apps.insert("png", "eog");
-    apps.insert("gif", "eog");
-    apps.insert("bmp", "eog");
-    apps.insert("tiff", "eog");
-    apps.insert("jpeg", "eog");
-    apps.insert("mp3", "vlc");
-    apps.insert("wav", "vlc");
-    apps.insert("flac", "vlc");
-    apps.insert("aac", "vlc");
-    apps.insert("mp4", "vlc");
-    apps.insert("avi", "vlc");
-    apps.insert("mkv", "vlc");
-    apps.insert("mov", "vlc");
-    apps.insert("zip", "xdg-open");
-    apps.insert("tar.gz", "xdg-open");
-    apps.insert("rar", "xdg-open");
-    apps.insert("7z", "xdg-open");
-    apps.insert("db", "sqlite3");
-    apps.insert("sqlite", "sqlite3");
-    apps.insert("docx", "libreoffice");
-    apps.insert("xlsx", "libreoffice");
-    apps.insert("pptx", "libreoffice");
-    apps.insert("bin", "hex_editor");
-    apps.insert("rs", "nvim");
-    apps.insert("sh", "nvim");
-    apps.get(extension).copied()
+    match extension.to_lowercase().as_str() {
+        "pdf" => Some("zathura"),
+        "jpg" | "jpeg" | "png" | "gif" | "bmp" | "tiff" => Some("eog"),
+        "mp3" | "wav" | "flac" | "aac" | "mp4" | "avi" | "mkv" | "mov" => Some("vlc"),
+        "zip" | "tar.gz" | "rar" | "7z" => Some("xdg-open"),
+        "db" | "sqlite" => Some("sqlite3"),
+        "docx" | "xlsx" | "pptx" => Some("libreoffice"),
+        "bin" => Some("hexeditor"),
+        "rs" | "sh" | "lua" | "config" | "c" => Some("nvim"),
+        _ => None,
+    }
 }
 
 fn prompt_open_with_nvim(file: &str) -> io::Result<()> {
