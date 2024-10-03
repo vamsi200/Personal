cb68aa2c9d1664eab501445c5f2d7cb882fa4045
vamsi200 <vamsivanama891@gmail.com>
2024-10-03 20:24:19 +0530

    Added Xclip & improved the code

--- tools/fzf/target/release/deps/fzf-53b000d04aeb28d1.d
+++
@@ -1,5 +0,0 @@
-/home/vamsi/scripts/Personal/tools/fzf/target/release/deps/fzf-53b000d04aeb28d1: src/main.rs
-
-/home/vamsi/scripts/Personal/tools/fzf/target/release/deps/fzf-53b000d04aeb28d1.d: src/main.rs
-
-src/main.rs:
--- tools/fzf/target/release/fzf.d
+++
@@ -1,1 +0,0 @@
-/home/vamsi/scripts/Personal/tools/fzf/target/release/fzf: /home/vamsi/scripts/Personal/tools/fzf/src/main.rs
