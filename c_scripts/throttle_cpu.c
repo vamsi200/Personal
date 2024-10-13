cb59d73286fea0f4ca9923b501ab670e6488b5d6
vamsi200 <vamsivanama891@gmail.com>
2024-10-13 19:57:13 +0530

    Added handling of Ctrl+c to reset to original cpu frequency & made few variable(name) changes

--- c_scripts/throttle_cpu.c
+++ c_scripts/throttle_cpu.c
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,147 +9,136 @@
 #define WAIT_TIME_AFTER_TEMP_DROP 30
 #define MAX_ATTEMPTS 5
 
-float cpu_temp;
+volatile sig_atomic_t keep_running = 1;
 
-float get_cpu_temp(void) {
-  FILE *open_file = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
-  if (open_file == NULL) {
+float get_current_cpu_temp(void) {
+  FILE *temp_file = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
+  if (temp_file == NULL) {
     perror("Error: Couldn't Read CPU Temperature");
     return -1;
-  } else {
-    fscanf(open_file, "%f", &cpu_temp);
-    fclose(open_file);
-    return cpu_temp / 1000.00;
   }
+  float cpu_temp;
+  fscanf(temp_file, "%f", &cpu_temp);
+  fclose(temp_file);
+  return cpu_temp / 1000.00;
 }
 
 int get_current_cpu_frequency(int cpu) {
-  char current_cpu_frequency[256];
-  snprintf(current_cpu_frequency, sizeof(current_cpu_frequency),
+  char frequency_file_path[256];
+  snprintf(frequency_file_path, sizeof(frequency_file_path),
            "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_max_freq", cpu);
 
-  FILE *fp_max = fopen(current_cpu_frequency, "r");
-  if (fp_max == NULL) {
+  FILE *frequency_file = fopen(frequency_file_path, "r");
+  if (frequency_file == NULL) {
     perror("Error: Couldn't Read CPU Frequency");
     return -1;
   }
 
-  int max_scaling_freq;
-  if (fscanf(fp_max, "%d", &max_scaling_freq) != 1) {
-    perror("Error: reading max scaling frequency");
-    fclose(fp_max);
+  int current_max_frequency;
+  if (fscanf(frequency_file, "%d", &current_max_frequency) != 1) {
+    perror("Error: reading current max frequency");
+    fclose(frequency_file);
     return -1;
   }
 
-  fclose(fp_max);
-  return max_scaling_freq;
+  fclose(frequency_file);
+  return current_max_frequency;
 }
 
-int change_cpu_frequency(int cpu, int frequency) {
-  char current_cpu_frequency[256];
-  snprintf(current_cpu_frequency, sizeof(current_cpu_frequency),
+int set_cpu_frequency(int cpu, int frequency) {
+  char frequency_file_path[256];
+  snprintf(frequency_file_path, sizeof(frequency_file_path),
            "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_max_freq", cpu);
 
-  FILE *fp_set = fopen(current_cpu_frequency, "w");
-  if (fp_set == NULL) {
-    perror("Error: Couldn't Set Throttled Frequency");
+  FILE *frequency_file = fopen(frequency_file_path, "w");
+  if (frequency_file == NULL) {
+    perror("Error: Couldn't Set Frequency");
     return -1;
   }
 
-  fprintf(fp_set, "%d", frequency);
-  fclose(fp_set);
+  fprintf(frequency_file, "%d", frequency);
+  fclose(frequency_file);
   return 0;
 }
 
 int get_original_cpu_frequency(int cpu) {
-  char get_cpumax_fq_info[256];
-  snprintf(get_cpumax_fq_info, sizeof(get_cpumax_fq_info),
+  char original_frequency_file_path[256];
+  snprintf(original_frequency_file_path, sizeof(original_frequency_file_path),
            "/sys/devices/system/cpu/cpu%d/cpufreq/cpuinfo_max_freq", cpu);
-  FILE *fp = fopen(get_cpumax_fq_info, "r");
+  FILE *frequency_file = fopen(original_frequency_file_path, "r");
 
-  if (fp == NULL) {
-    perror("Error: Couldn't read `CPUMAX_FQ_INFO`");
+  if (frequency_file == NULL) {
+    perror("Error: Couldn't read `cpuinfo_max_freq`");
     return -1;
   }
   int original_cpu_freq;
-  if (fscanf(fp, "%d", &original_cpu_freq) != 1) {
-    perror("Error: Couldn't read `Original_Cpu_Freq`");
-    fclose(fp);
+  if (fscanf(frequency_file, "%d", &original_cpu_freq) != 1) {
+    perror("Error: Couldn't read original frequency");
+    fclose(frequency_file);
     return -1;
   }
 
-  fclose(fp);
+  fclose(frequency_file);
   return original_cpu_freq;
 }
 
-int set_to_original_frequency(int cpu, int frequency) {
-  char scaling_max_fq_info[256];
-  snprintf(scaling_max_fq_info, sizeof(scaling_max_fq_info),
-           "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_max_freq", cpu);
-  FILE *fp = fopen(scaling_max_fq_info, "w");
-  if (fp == NULL) {
-    perror("Error: Couldn't write `scaling_max_freq`");
-    return -1;
-  }
-  fprintf(fp, "%d", frequency);
-  fclose(fp);
-  return 0;
-}
+int throttle(int temperature_limit) {
+  int max_cpu_cores = sysconf(_SC_NPROCESSORS_ONLN);
+  float current_cpu_temp = get_current_cpu_temp();
+  int throttle_attempts = 0;
 
-int throttle(int temperature) {
-  int max_cpu = sysconf(_SC_NPROCESSORS_ONLN);
-  float cpu_temp = get_cpu_temp();
-  int attempt = 0;
+  while (current_cpu_temp > temperature_limit &&
+         throttle_attempts < MAX_ATTEMPTS && keep_running) {
+    printf("[*] Throttle attempt %d/%d\n", throttle_attempts + 1, MAX_ATTEMPTS);
 
-  while (cpu_temp > temperature && attempt < MAX_ATTEMPTS) {
-    printf("[*] Throttle attempt %d/%d\n", attempt + 1, MAX_ATTEMPTS);
-
-    for (int cpu = 0; cpu < max_cpu; cpu++) {
-      int max_freq = get_current_cpu_frequency(cpu);
-      if (max_freq < 0) {
+    for (int cpu = 0; cpu < max_cpu_cores; cpu++) {
+      int current_max_frequency = get_current_cpu_frequency(cpu);
+      if (current_max_frequency < 0) {
         continue;
       }
 
-      int throttle_freq = max_freq - FREQUENCY_DECREMENT;
-      if (throttle_freq < 0) {
-        throttle_freq = 0;
+      int new_frequency = current_max_frequency - FREQUENCY_DECREMENT;
+      if (new_frequency < 0) {
+        new_frequency = 0;
       }
 
-      printf("CPU%d: Throttling frequency from %d Hz to %d Hz\n", cpu, max_freq,
-             throttle_freq);
-      if (change_cpu_frequency(cpu, throttle_freq) < 0) {
+      printf("CPU%d: Throttling frequency from %d Hz to %d Hz\n", cpu,
+             current_max_frequency, new_frequency);
+      if (set_cpu_frequency(cpu, new_frequency) < 0) {
         continue;
       }
     }
 
-    printf("[*] Waiting for %d seconds, Current temperature:: %f...\n",
-           WAIT_TIME_AFTER_THROTTLE, cpu_temp);
+    printf("[*] Waiting for %d seconds, Current temperature: %f°C...\n",
+           WAIT_TIME_AFTER_THROTTLE, current_cpu_temp);
     sleep(WAIT_TIME_AFTER_THROTTLE);
 
-    cpu_temp = get_cpu_temp();
-    if (cpu_temp < 0) {
+    current_cpu_temp = get_current_cpu_temp();
+    if (current_cpu_temp < 0) {
       printf("Error: reading CPU temperature.\n");
       return -1;
     }
 
-    attempt++;
+    throttle_attempts++;
   }
 
-  if (cpu_temp <= temperature) {
+  if (!keep_running) {
+    return -1; // If Ctrl + C is pressed, exit the throttle loop
+  }
 
-    printf("[*] Temperature is below the limit set. Waiting for %d seconds to "
+  if (current_cpu_temp <= temperature_limit) {
+    printf("[*] Temperature is below the limit. Waiting for %d seconds to "
            "confirm...\n",
            WAIT_TIME_AFTER_TEMP_DROP);
     sleep(WAIT_TIME_AFTER_TEMP_DROP);
-    cpu_temp = get_cpu_temp();
+    current_cpu_temp = get_current_cpu_temp();
 
-    if (cpu_temp <= temperature) {
+    if (current_cpu_temp <= temperature_limit) {
       printf("[*] Temperature is stable. Unthrottling...\n");
       return 1;
     } else {
-      printf("[*] Temperature's still above limit set. Continuing "
-             "throttling...\n");
-      return throttle(temperature);
+      printf("[*] Temperature's still above limit. Continuing throttling...\n");
+      return throttle(temperature_limit);
     }
   } else {
     printf("[!] Maximum attempts reached. Unthrottling and exiting...\n");
@@ -157,49 +147,59 @@ int throttle(int temperature) {
 }
 
 void unthrottle() {
-  int max_cpu = sysconf(_SC_NPROCESSORS_ONLN);
+  int max_cpu_cores = sysconf(_SC_NPROCESSORS_ONLN);
 
-  for (int cpu = 0; cpu < max_cpu; cpu++) {
+  for (int cpu = 0; cpu < max_cpu_cores; cpu++) {
     int original_frequency = get_original_cpu_frequency(cpu);
     if (original_frequency < 0) {
       continue;
     }
-    printf("CPU%d: Restoring frequency to original %d Hz\n", cpu,
+    printf("[*] CPU%d: Restoring frequency to original %d Hz\n", cpu,
            original_frequency);
 
-    if (set_to_original_frequency(cpu, original_frequency) < 0) {
+    if (set_cpu_frequency(cpu, original_frequency) < 0) {
       continue;
     }
   }
 }
 
+void handle_sigint(int sig) {
+  printf("\n[*] Caught signal %d (Ctrl + C). Unthrottling and exiting...\n",
+         sig);
+  unthrottle();
+  exit(0);
+}
+
 int main(int argc, char *argv[]) {
+  signal(SIGINT, handle_sigint);
+
   if (argc != 2) {
     printf("Usage: sudo %s <Temperature_Limit>\n", argv[0]);
     exit(1);
   }
 
-  int input_temp = atoi(argv[1]);
-  if (input_temp == 0 && argv[1][0] != '0') {
+  int temperature_limit = atoi(argv[1]);
+  if (temperature_limit == 0 && argv[1][0] != '0') {
     printf("Invalid input. Please provide a valid integer.\n");
     exit(1);
   }
 
-  float temp = get_cpu_temp();
-  if (temp < 0) {
+  float current_cpu_temp = get_current_cpu_temp();
+  if (current_cpu_temp < 0) {
     printf("Error: reading CPU temperature.\n");
     return 1;
   }
 
-  if (temp > input_temp) {
+  if (current_cpu_temp > temperature_limit) {
     printf("[*] Starting Throttle...\n");
-    int success = throttle(input_temp);
-    if (!success) {
+    int success = throttle(temperature_limit);
+    if (!success && keep_running) {
       printf("[!] Throttling failed after max attempts. Unthrottling...\n");
+      unthrottle();
     }
-    unthrottle();
   } else {
-    printf("[*] CPU Temperature is under set limit: %.2f°C\n", temp);
+    printf("[*] CPU Temperature is under set limit: %.2f°C\n",
+           current_cpu_temp);
   }
 
   return 0;
