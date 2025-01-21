592a9321cf4db30485368297f8ae56f49445437c
vamsi200 <vamsivanama891@gmail.com>
2025-01-21 12:46:40 +0530

    nmap scan script

--- bash_scripts/scan.sh
+++ bash_scripts/scan.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
 export DISPLAY=:0
 export XDG_RUNTIME_DIR=/run/user/1000
 
-get_ports=$(nmap -sC -sV -p- -T4 "localhost" -oN "$HOME/temp/scan_$(date +'%Y-%m-%d').txt")
-port=$(grep -w open $HOME/temp/scan* | grep -v '^|_')
+file="$HOME/temp/scan_$(date +'%Y-%m-%d').txt"
+
+nmap_scan=$(nmap -sC -sV -p- -T4 "localhost" -oN "$file")
+
+port=$(grep -w open $file | grep -v '^|_')
+
 zenity --info --text="$port" --width=350 --height=200
+
